fd8620be1d0e06d4ebab17b636704275c775250d
jasongzj <jasongzj@163.com>
2018-11-30 08:40:05 +0000

    add file header

--- src/QcloudImage.php
+++ src/QcloudImage.php
@@ -1,9 +1,11 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: jason
- * Date: 2018/11/22
- * Time: 0:09
+ * This file is part of the jasongzj/laravel-qcloud-image.
+ *
+ * (c) jasongzj <jasongzj@163.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace Jasongzj\LaravelQcloudImage;
